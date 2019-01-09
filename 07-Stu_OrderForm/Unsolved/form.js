1c1310cfb907cf7af32d534a9a9c1d74a254476e
Unknown <sayhellocelina@gmail.com>
2019-01-08 21:30:05 -0800

    solved for 06 & 07

--- 07-Stu_OrderForm/Unsolved/form.js
+++ 07-Stu_OrderForm/Unsolved/form.js
@@ -1,12 +1,36 @@
-// YOUR CODE HERE
-// 1. Event listener
+$("#submitOrder").click(function () {
 
-  // 2. Retrieve input values
-
-  // 3. For each variable: Check if blank,
-  // if blank set to 0,
-  // else parse as integer
-
-  // 4. Add all variables together
-
-  // 5. Alter modal content
+  // Retrieve input values
+  var numApple = $("#apple").val()
+  var numPecan = $("#pecan").val()
+  var numKeyLime = $("#keyLime").val()
+ 
+  // Check if blank, if blank set to 0, else parse as integer
+  if (numApple == "") {
+    numApple = 0;
+  }
+  else {
+    numApple = parseInt(numApple);
+  }
+ 
+  if (numPecan == "") {
+    numPecan = 0;
+  }
+  else {
+    numPecan = parseInt(numPecan);
+  }
+ 
+  if (numKeyLime == "") {
+    numKeyLime = 0;
+  }
+  else {
+    numKeyLime = parseInt(numKeyLime)
+  }
+ 
+  // Add
+  var totalPies = numApple + numPecan + numKeyLime;
+ 
+  // change modal content
+  $(".modal-body").html("Please confirm your order for " + totalPies + " pies.");
+ 
+ });
